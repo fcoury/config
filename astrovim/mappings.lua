3e56a21e0d16f9b26e30b77c09405a1b6521e594
Felipe Coury <felipe.coury@gmail.com>
2023-03-13 20:18:10 -0300

    feat: added Ctrl+X as terminal shortcut on astrovim

--- astrovim/mappings.lua
+++ astrovim/mappings.lua
@@ -1,5 +1,9 @@
 return {
   n = {
     [";"] = { ":", desc = "Remaps ; to :" },
+    ["<C-x>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" }
   },
+  t = {
+    ["<C-x>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" }
+  }
 }
