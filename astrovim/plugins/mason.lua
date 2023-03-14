1d31157ba21948c3a83fecbed733c8170ece62f6
Felipe Coury <felipe.coury@gmail.com>
2023-03-14 00:11:49 -0300

    feat: auto installs lsp for lua and rust

---
+++ astrovim/plugins/mason.lua
@@ -0,0 +1,12 @@
+return {
+  { "williamboman/mason.nvim", opts = { PATH = "append" } },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "rust_analyzer"
+      }
+    }
+  }
+}
