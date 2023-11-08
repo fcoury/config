2e0dfa93e831e8ebf409fa9f7e2d7c8874164413
Felipe Coury <felipe.coury@gmail.com>
2023-11-08 17:48:24 -0300

    chore: renamed astrovim to astronvim

---
+++ astronvim/plugins/crates.lua
@@ -0,0 +1,9 @@
+return {
+  "saecki/crates.nvim",
+  tag = 'v0.3.0',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('crates').setup()
+  end,
+  event = { "BufRead Cargo.toml" }
+}
