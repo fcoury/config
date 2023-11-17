b811015b1a78fe9cc687808a46153fa5a24dd6e0
Felipe Coury <felipe.coury@gmail.com>
2023-11-17 12:12:44 -0300

    astronvim: added custom command to toggle root based on a .roots file

--- astronvim/init.lua
+++ astronvim/init.lua
@@ -1,3 +1,7 @@
+-- user defined command
+local toggle = require "user.commands.toggle"
+vim.api.nvim_create_user_command("ToggleRoot", toggle.toggleRootPath, {})
+
 return {
   -- colorscheme = "nightfox",
   colorscheme = "tokyonight-night",
--- astronvim/mappings.lua
+++ astronvim/mappings.lua
@@ -7,11 +7,13 @@ return {
     ["<Leader>fp"] = { "<cmd>Telescope projects<CR>", desc = "Open project search" },
     ["<Leader>ln"] = { "<cmd>RustRunnables<cr>", desc = "Rust Runnables" },
     ["<Leader>lx"] = { require("rust-tools").hover_actions.hover_actions(), desc = "Rust hover actions" },
+    ["<C-P>"] = { "<cmd>ToggleRoot<CR>", desc = "Toggles between project roots defined by .roots" },
     ["_"] = { "<cmd>split<cr>", desc = "Horizontal split" },
     ["|"] = { "<cmd>vsplit<cr>", desc = "Vertical split" },
     -- disabled mappings
     ["<C-Down>"] = false,
     ["<C-Up>"] = false,
+    -- ["<C-x>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
   },
   i = {
     ["<C-n>"] = false,
