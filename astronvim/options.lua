86efa8ddc4485b9106e44eb412946fceb12ee364
Felipe Coury <felipe.coury@gmail.com>
2023-11-18 00:10:09 -0300

    astronvim: comments formatting

--- astronvim/options.lua
+++ astronvim/options.lua
@@ -2,6 +2,12 @@ return {
   opt = {
     relativenumber = true, -- show relative line numbers
     number = true,         -- show line numbers
+
+    -- tc = wrap text and comments using textwidth,
+    -- r = continue comments when pressing enter,
+    -- q = continue comments when pressing o or O,
+    -- n = indent past formatlistpat, not gq
+    formatoptions = "tcrqn",
   },
   -- g = {
   --   mapleader = ",",       -- set leader key to ,
