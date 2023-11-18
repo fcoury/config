352907694a25ba8164d5aa432c4d0e8f49f50728
Felipe Coury <felipe.coury@gmail.com>
2023-11-18 01:25:43 -0300

    astronvim: new statusline after char

--- astronvim/init.lua
+++ astronvim/init.lua
@@ -18,7 +18,7 @@ return {
 
   heirline = {
     separators = {
-      left = { "", " " }, -- separator for the left side of the statusline
+      left = { "", " " }, -- separator for the left side of the statusline
       right = { " ", "" }, -- separator for the right side of the statusline
       tab = { "", "" },
     },
