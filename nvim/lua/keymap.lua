d381676df0bb77a10d86b9d893e277519a9b05ba
Felipe Coury <felipe.coury@gmail.com>
2024-02-15 15:03:24 -0300

    feat(nvim): new keymappings

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -14,11 +14,11 @@ keymap.set("n", "L", "$", { noremap = true })
 -- telescope
 keymap.set("n", "<leader>d", "<cmd>Telescope diagnostics<cr>")
 keymap.set("n", "<leader>s", "<cmd>Telescope lsp_document_symbols<cr>")
-keymap.set("n", "<C-r>", "<cmd>Telescope lsp_document_symbols<cr>")
+keymap.set("n", "<C-t>", "<cmd>Telescope lsp_document_symbols<cr>")
 
 -- lsp and rust
 keymap.set("n", "<leader>x", "<cmd>RustRunnables<cr>")
-keymap.set("n", "<leader>y", "<cmd>lua require'runst'.run_test()<cr>", { noremap = true, silent = true })
+-- keymap.set("n", "<leader>y", "<cmd>lua require'runst'.run_test()<cr>", { noremap = true, silent = true })
 
 -- buffers
 keymap.set("n", "]b", "<cmd>bnext<cr>")
