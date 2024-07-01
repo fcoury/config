78d167c44a1b7605963b0cb5732cf88ba6f435c4
Felipe Coury <felipe.coury@gmail.com>
2024-07-01 12:24:26 -0300

    feat(nvim): disabled runst and harpoon

---
+++ nvim/lua/disabled/harpoon.lua
@@ -0,0 +1,53 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
+
+		-- basic telescope configuration
+		local conf = require("telescope.config").values
+		local function toggle_telescope(harpoon_files)
+			local file_paths = {}
+			for _, item in ipairs(harpoon_files.items) do
+				table.insert(file_paths, item.value)
+			end
+
+			require("telescope.pickers")
+				.new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({
+						results = file_paths,
+					}),
+					previewer = conf.file_previewer({}),
+					sorter = conf.generic_sorter({}),
+				})
+				:find()
+		end
+
+		vim.keymap.set("n", "<C-1>", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<C-2>", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<C-3>", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<C-4>", function()
+			harpoon:list():select(4)
+		end)
+		vim.keymap.set("n", "<leader>m", function()
+			harpoon:list():add()
+			print("added to list")
+		end)
+		vim.keymap.set("n", "<leader>n", function()
+			harpoon:list():remove()
+			print("removed from list")
+		end)
+		vim.keymap.set("n", "<C-m>", function()
+			toggle_telescope(harpoon:list())
+		end)
+	end,
+}
