a61f412b5f3faeda84f1c796c06a1be6dc168745
Felipe Coury <felipe.coury@gmail.com>
2024-07-01 22:10:05 -0300

    feat(nvim): vial config

--- nvim/lua/plugins/vial.lua
+++ nvim/lua/plugins/vial.lua
@@ -2,6 +2,12 @@ return {
 	"fcoury/vial.nvim",
 	dir = "~/code/vial",
 	config = function()
-		require("vial").setup()
+		require("vial").setup({
+			vial_path = "/Users/fcoury/code/vial/target/debug/vial",
+			-- watchexec -c -r -e rs -- cargo $cmd
+			-- wg 'test test_lex_struct_field_set -- --nocapture'
+			command = "cargo test %s -- --nocapture --color=always",
+			extensions = { ".rs" },
+		})
 	end,
 }
