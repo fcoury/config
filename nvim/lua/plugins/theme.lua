dc8e19f59308d95aec3f037faea571d3b6566a2d
Felipe Coury <felipe.coury@gmail.com>
2024-08-28 17:00:10 -0300

    chore(nvim): changed catpuccin theme to mocha version

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -4,16 +4,6 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("catppuccin-frappe")
-		-- require("catppuccin").setup({
-		-- 	flavor = "frappe",
-		-- 	integrations = {
-		-- 		cmp = true,
-		-- 		gitsigns = true,
-		-- 		nvimtree = true,
-		-- 		treesitter = true,
-		-- 		fidget = true,
-		-- 	},
-		-- })
+		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
