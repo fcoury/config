5a5dc5cee6bd619a94593b1385b4e4486fefa434
Felipe Coury <felipe.coury@gmail.com>
2024-08-28 12:14:17 -0300

    feat(nvim): uses telescope-live-grep-args

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,9 +8,11 @@ return {
 			local builtin = require("telescope.builtin")
 			local keymap = vim.keymap
 			keymap.set("n", "<C-p>", builtin.find_files, {})
-			keymap.set("n", "<leader>g", builtin.live_grep, {})
+			-- keymap.set("n", "<leader>g", builtin.live_grep, {})
+			keymap.set("n", "<leader>g", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", {})
 
 			local actions = require("telescope.actions")
+			local lga_actions = require("telescope-live-grep-args.actions")
 			require("telescope").setup({
 				defaults = {
 					mappings = {
@@ -51,6 +53,24 @@ return {
 						preview_width = 0.5,
 					},
 				},
+				extensions = {
+					live_grep_args = {
+						auto_quoting = true, -- enable/disable auto-quoting
+						-- define mappings, e.g.
+						mappings = { -- extend mappings
+							i = {
+								["<C-k>"] = lga_actions.quote_prompt(),
+								["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+								-- freeze the current list and start a fuzzy search in the frozen list
+								["<C-space>"] = actions.to_fuzzy_refine,
+							},
+						},
+						-- ... also accepts theme settings, for example:
+						-- theme = "dropdown", -- use dropdown theme
+						-- theme = { }, -- use own theme spec
+						-- layout_config = { mirror=true }, -- mirror preview pane
+					},
+				},
 			})
 		end,
 	},
@@ -83,6 +103,12 @@ return {
 			-- To get ui-select loaded and working with telescope, you need to call
 			-- load_extension, somewhere after setup function:
 			require("telescope").load_extension("ui-select")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-live-grep-args.nvim",
+		config = function()
+			require("telescope").load_extension("live_grep_args")
 		end,
 	},
 }
