3e10026341737f7e9a2187b93a9aebc566afc5fd
Felipe Coury <felipe.coury@gmail.com>
2024-09-13 23:48:42 -0300

    chore(nvim): commands for nvim-surround

--- nvim/lua/plugins/nvim-surround.lua
+++ nvim/lua/plugins/nvim-surround.lua
@@ -6,3 +6,30 @@ return {
 		require("nvim-surround").setup({})
 	end,
 }
+
+-- Insert Surroundings:
+-- ysiw': Surround the inner word with single quotes ('word').
+-- ysiw": Surround the inner word with double quotes ("word").
+-- ysiw(: Surround the inner word with parentheses ((word)).
+-- ysiw{: Surround the inner word with curly braces ({word}).
+-- ysiw[: Surround the inner word with square brackets ([word]).
+-- ysiw<: Surround the inner word with angle brackets (<word>).
+--
+-- Change Surroundings:
+-- cs'": Change surrounding single quotes ('word') to double quotes ("word").
+-- cs([{: Change surrounding parentheses ((word)) to curly braces ({word}).
+-- cs"{: Change surrounding double quotes ("word") to curly braces ({word}).
+--
+-- Delete Surroundings:
+-- ds': Delete surrounding single quotes ('word' → word).
+-- ds": Delete surrounding double quotes ("word" → word).
+-- ds(: Delete surrounding parentheses ((word) → word).
+--
+-- Insert Surroundings for Visual Selections:
+-- S': Surround visually selected text with single quotes.
+-- S": Surround visually selected text with double quotes.
+-- S(: Surround visually selected text with parentheses.
+--
+-- Insert Surroundings Line-Wise:
+-- yss': Surround the entire line with single quotes.
+-- yss": Surround the entire line with double quotes.
