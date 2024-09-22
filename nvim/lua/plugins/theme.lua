f94c0fcd1b1929d32874faa77521cef5bacd7bbd
Felipe Coury <felipe.coury@gmail.com>
2024-09-22 14:33:32 -0300

    feat: theme picker

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -20,15 +20,83 @@
 
 -- lackluster
 return {
-	"slugbyte/lackluster.nvim",
-	lazy = false,
-	priority = 1000,
-	init = function()
-		-- vim.cmd.colorscheme("lackluster")
-		vim.cmd.colorscheme("lackluster")
-		-- vim.cmd.colorscheme("lackluster-mint")
-		-- vim.cmd.colorscheme("lackluster-night")
-	end,
+	{
+		"slugbyte/lackluster.nvim",
+		lazy = false,
+		priority = 1000,
+		init = function()
+			-- vim.cmd.colorscheme("lackluster")
+			-- vim.cmd.colorscheme("lackluster")
+			-- vim.cmd.colorscheme("lackluster-mint")
+			-- vim.cmd.colorscheme("lackluster-night")
+		end,
+	},
+	{ "cocopon/iceberg.vim" },
+	{ "sainnhe/everforest" },
+	{ "rebelot/kanagawa.nvim" },
+	{ "EdenEast/nightfox.nvim" },
+	{ "rose-pine/neovim", as = "rose-pine" },
+	{ "morhetz/gruvbox" },
+	{ "AlexvZyl/nordic.nvim" },
+	{ "catppuccin/nvim" },
+	{
+		"zaldih/themery.nvim",
+		lazy = false,
+		config = function()
+			require("themery").setup({
+				themes = {
+					"lackluster",
+					"lackluster-dark",
+					"lackluster-hack",
+					"lackluster-mint",
+					"lackluster-night",
+					"iceberg",
+					"everforest",
+					"kanagawa",
+					"kanagawa-dragon",
+					"kanagawa-wave",
+					"kanagawa-lotus",
+					"nightfox",
+					"terafox",
+					"rose-pine-main",
+					"rose-pine-dawn",
+					"rose-pine-moon",
+					"nordic",
+					"gruvbox",
+					"catppuccin-mocha",
+					"catppuccin-frappe",
+					"catppuccin-macchiato",
+					"catppuccin-latte",
+				},
+				livePreview = true,
+			})
+
+			vim.api.nvim_create_autocmd("ColorScheme", {
+				pattern = "*",
+				callback = function()
+					vim.api.nvim_set_hl(0, "LspInlayHint", {
+						fg = vim.api.nvim_get_hl_by_name("LspInlayHint", true).foreground, -- Retain the current foreground color
+						bg = "NONE", -- Set background to NONE for transparency
+						blend = vim.api.nvim_get_hl_by_name("LspInlayHint", true).blend, -- Retain the current blend value
+					})
+				end,
+			})
+
+			-- vim.api.nvim_create_autocmd("ColorScheme", {
+			-- 	pattern = "*",
+			-- 	callback = function()
+			-- 		local current_colorscheme = vim.g.colors_name or ""
+			-- 		if current_colorscheme:match("^rose%-pine") then
+			-- 			vim.api.nvim_set_hl(0, "LspInlayHint", {
+			-- 				fg = "#6e6a86", -- Keep the original foreground color
+			-- 				bg = "NONE", -- Set background to NONE for transparency
+			-- 				blend = 10, -- Keep the original blend value
+			-- 			})
+			-- 		end
+			-- 	end,
+			-- })
+		end,
+	},
 }
 
 -- kanagawa
