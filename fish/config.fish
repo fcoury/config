b192adf5a747e7a48173a2ba261e50e3968aef18
Felipe Coury <felipe.coury@gmail.com>
2024-09-30 00:44:25 -0300

    fix: TERM set to xterm-256 color

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ end
 set -gx EDITOR /opt/homebrew/bin/nvim
 set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
 set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
-set -gx TERM "tmux-256color"
+set -gx TERM "xterm-256color"
 
 # pnpm
 set -gx PNPM_HOME "/Users/fcoury/Library/pnpm"
