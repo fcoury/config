6abefd4236fa3209107d118b554a03526a54daa7
Felipe Coury <felipe.coury@gmail.com>
2024-09-30 00:45:58 -0300

    feat: descriptions on keymap items

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -3,40 +3,38 @@ local keymap = vim.keymap
 -- remaps ; to act as :
 keymap.set("n", ";", ":", { noremap = true })
 
-keymap.set("n", "<leader>a", "ggVG") -- select all
-keymap.set({ "n", "x" }, "<leader>p", '"0p') -- paste not overwritten by delete
--- keymap.set("n", "<leader>w", "<cmd>w<cr>") -- save current file
-keymap.set("n", "<leader>q", "<cmd>q<cr>") -- quit
-keymap.set("n", "U", "<C-r>", { noremap = true })
-keymap.set("n", "H", "^", { noremap = true })
-keymap.set("n", "L", "$", { noremap = true })
+keymap.set("n", "<leader>a", "ggVG", { desc = "select all" }) -- select all
+keymap.set({ "n", "x" }, "<leader>p", '"0p', { desc = "paste and doesn't overwrite" }) -- paste not overwritten by delete
+keymap.set("n", "U", "<C-r>", { noremap = true, desc = "redo" })
+keymap.set("n", "H", "^", { noremap = true, desc = "move to beginning of line" })
+keymap.set("n", "L", "$", { noremap = true, desc = "move to end of line" })
 
 -- telescope
-keymap.set("n", "<C-j>", "<cmd>Telescope buffers<cr>")
-keymap.set("n", "<leader>b", "<cmd>Telescope buffers<cr>")
-keymap.set("n", "<leader>d", "<cmd>Telescope diagnostics<cr>")
-keymap.set("n", "<leader>s", "<cmd>Telescope lsp_document_symbols<cr>")
-keymap.set("n", "<leader>w", "<cmd>Telescope lsp_workspace_symbols<cr>")
-keymap.set("n", "<leader>k", "<cmd>Telescope lsp_references<cr>")
-keymap.set("n", "<C-t>", "<cmd>Telescope lsp_document_symbols<cr>")
+keymap.set("n", "<C-j>", "<cmd>Telescope buffers<cr>", { desc = "open buffers" })
+keymap.set("n", "<leader>b", "<cmd>Telescope buffers<cr>", { desc = "open buffers" })
+keymap.set("n", "<leader>d", "<cmd>Telescope diagnostics<cr>", { desc = "open diagnostics" })
+keymap.set("n", "<leader>s", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "open document symbols" })
+keymap.set("n", "<leader>w", "<cmd>Telescope lsp_workspace_symbols<cr>", { desc = "open workspace symbols" })
+keymap.set("n", "<leader>k", "<cmd>Telescope lsp_references<cr>", { desc = "open references" })
+keymap.set("n", "<C-t>", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "open document symbols" })
 
 -- lsp and rust
-keymap.set("n", "<leader>x", "<cmd>RustRunnables<cr>")
+keymap.set("n", "<leader>x", "<cmd>RustRunnables<cr>", { desc = "run rust runnables" })
 -- keymap.set("n", "<leader>y", "<cmd>lua require'runst'.run_test()<cr>", { noremap = true, silent = true })
 
 -- buffers
-keymap.set("n", "]b", "<cmd>bnext<cr>")
-keymap.set("n", "[b", "<cmd>bprevious<cr>")
+keymap.set("n", "]b", "<cmd>bnext<cr>", { desc = "next buffer" })
+keymap.set("n", "[b", "<cmd>bprevious<cr>", { desc = "previous buffer" })
 -- keymap.set("n", "<leader>c", "<cmd>bd<cr>")
-keymap.set("n", "<leader><leader>", "<cmd>b#<cr>")
+keymap.set("n", "<leader><leader>", "<cmd>b#<cr>", { desc = "switch to last buffer" })
 
 -- splits
-keymap.set("n", "|", "<cmd>vsplit<cr><c-w><c-w>")
-keymap.set("n", "_", "<cmd>split<cr>")
-keymap.set("n", "=", "<cmd>wincmd =<cr>")
+keymap.set("n", "|", "<cmd>vsplit<cr><c-w><c-w>", { desc = "split vertically" })
+keymap.set("n", "_", "<cmd>split<cr>", { desc = "split horizontally" })
+keymap.set("n", "=", "<cmd>wincmd =<cr>", { desc = "balance splits" })
 
 -- lsp related
-keymap.set("n", "<leader>,d", "<cmd>DiagnosticToggle<cr>")
+keymap.set("n", "<leader>,d", "<cmd>DiagnosticToggle<cr>", { desc = "toggle diagnostics" })
 
 -- special commands
 keymap.set(
@@ -47,11 +45,11 @@ keymap.set(
 )
 
 -- delete text (as opposed to saving it on the default register)
-vim.api.nvim_set_keymap("n", "m", '"_x', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "mm", '"_dd', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "m", '"_x', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "mw", '"_dw', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "miw", '"_diw', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "m", '"_x', { noremap = true, silent = true, desc = "delete text" })
+vim.api.nvim_set_keymap("n", "mm", '"_dd', { noremap = true, silent = true, desc = "delete line" })
+vim.api.nvim_set_keymap("v", "m", '"_x', { noremap = true, silent = true, desc = "delete text" })
+vim.api.nvim_set_keymap("n", "mw", '"_dw', { noremap = true, silent = true, desc = "delete word" })
+vim.api.nvim_set_keymap("n", "miw", '"_diw', { noremap = true, silent = true, desc = "delete inner word" })
 
 --== Helper Functions ==--
 
