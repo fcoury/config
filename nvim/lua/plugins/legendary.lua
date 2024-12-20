14a0978b4878123207d69fdf45593e060b4bdfa7
Felipe Coury <felipe.coury@gmail.com>
2024-12-20 14:34:14 -0300

    feat(nvim): diq and ciq as maps to delete and change inside quotes

--- nvim/lua/plugins/legendary.lua
+++ nvim/lua/plugins/legendary.lua
@@ -76,6 +76,8 @@ return {
 			{ "m", '"_x', mode = "v", opts = { noremap = true, silent = true }, description = "Delete text" },
 			{ "mw", '"_dw', mode = "n", opts = { noremap = true, silent = true }, description = "Delete word" },
 			{ "miw", '"_diw', mode = "n", opts = { noremap = true, silent = true }, description = "Delete inner word" },
+			{ "diq", 'di"', mode = "n", opts = { noremap = true, silent = true }, description = "Delete inside quote" },
+			{ "ciq", 'ci"', mode = "n", opts = { noremap = true, silent = true }, description = "Change inside quote" },
 
 			-- conditional move
 			{
