aadb42dd64a327421eff153c0820618659ae6adb
Felipe Coury <felipe.coury@gmail.com>
2024-12-29 17:54:20 -0300

    feat(nvim): cloak and screenkey plugins

---
+++ nvim/lua/plugins/cloack.lua
@@ -0,0 +1,19 @@
+return {
+	"laytan/cloak.nvim",
+	config = function()
+		require("cloak").setup({
+			enabled = true,
+			cloak_length = 3,
+			cloak_character = "*",
+			highlight_group = "Comment",
+			try_all_patterns = true,
+			cloak_telescope = true,
+			patterns = {
+				{
+					file_pattern = ".env*",
+					cloak_pattern = "=.+",
+				},
+			},
+		})
+	end,
+}
