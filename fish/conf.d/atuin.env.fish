08ffcb6ba7ef87d0993e8ddeea0b0893022a6995
Felipe Coury <felipe.coury@gmail.com>
2025-01-06 18:23:37 -0300

    feat(fish): removed atuin bind for up arrow

--- fish/conf.d/atuin.env.fish
+++ fish/conf.d/atuin.env.fish
@@ -1,4 +1,5 @@
 if test -d "$HOME/.atuin"
   source "$HOME/.atuin/bin/env.fish"
-  atuin init fish --disable-up-arrow --disable-ctrl-r | source
+  # atuin init fish --disable-up-arrow --disable-ctrl-r | source
+  atuin init fish --disable-up-arrow | source
 end
