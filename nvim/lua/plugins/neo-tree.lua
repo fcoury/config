1382fa1fae104157a6eaa9f137d545c76a3b3607
Felipe Coury <felipe.coury@gmail.com>
2025-01-06 15:54:31 -0300

    feat(nvim): auto close on neo-tree file selection

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -3,9 +3,9 @@ return {
 	branch = "v3.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
-		"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		"3rd/image.nvim",
 	},
 	keys = {
 		{ "<C-e>", mode = { "n" }, ":Neotree filesystem toggle left<CR>", desc = "Toggle file tree", silent = true },
@@ -26,6 +26,18 @@ return {
 				follow_current_file = {
 					enabled = true,
 					leave_dirs_open = false,
+				},
+			},
+			-- auto close file on selection
+			event_handlers = {
+				{
+					event = "file_open_requested",
+					handler = function()
+						-- auto close
+						-- vim.cmd("Neotree close")
+						-- OR
+						require("neo-tree.command").execute({ action = "close" })
+					end,
 				},
 			},
 		})
