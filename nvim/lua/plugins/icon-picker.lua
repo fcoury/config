33f75733eaa4fd84e650a1e4d93f59bd15e6db82
Felipe Coury <felipe.coury@gmail.com>
2025-01-09 14:14:31 -0300

    feat(nvim): removed <leader>k for IconPicker

--- nvim/lua/plugins/icon-picker.lua
+++ nvim/lua/plugins/icon-picker.lua
@@ -5,7 +5,7 @@ return {
 
 		local opts = { noremap = true, silent = true }
 
-		vim.keymap.set("n", "<Leader>k", "<cmd>IconPickerNormal<cr>", opts)
+		-- vim.keymap.set("n", "<Leader>k", "<cmd>IconPickerNormal<cr>", opts)
 		-- vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
 		-- vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", opts)
 	end,
