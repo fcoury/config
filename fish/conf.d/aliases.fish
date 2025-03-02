a54b4a92237082e551d35f6510220401a3014efc
Felipe Coury <felipe.coury@gmail.com>
2025-03-02 14:32:37 -0300

    feat: tp for picking a tmux session

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -77,3 +77,47 @@ function tm
     tmux new-session -s "$session_name"
   end
 end
+
+function tp
+  # Check if tmux is running
+  if not command -q tmux
+    echo "tmux is not installed"
+    return 1
+  end
+
+  # If no tmux sessions exist, create one based on current directory
+  if test (tmux list-sessions 2>/dev/null | wc -l) -eq 0
+    set session_name (basename (pwd) | string replace '.' '_')
+    tmux new-session -s "$session_name"
+    return
+  end
+
+  # If no argument is given, use fzf to select a session
+  if test (count $argv) -eq 0
+    set selected_session (tmux list-sessions -F "#{session_name}" | fzf --height 40% --reverse)
+    
+    # If user selected a session, attach to it
+    if test -n "$selected_session"
+      tmux attach-session -t "$selected_session"
+    else
+      # If no session was selected (user pressed Esc), create a new one
+      set session_name (basename (pwd) | string replace '.' '_')
+      
+      if tmux has-session -t "$session_name" 2>/dev/null
+        tmux attach-session -t "$session_name"
+      else
+        tmux new-session -s "$session_name"
+      end
+    end
+  else
+    # If an argument is given, try to attach to that session or create it
+    set session_name $argv[1]
+    
+    if tmux has-session -t "$session_name" 2>/dev/null
+      tmux attach-session -t "$session_name"
+    else
+      tmux new-session -s "$session_name"
+    end
+  end
+end
+
