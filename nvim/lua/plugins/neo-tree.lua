39dd069971cb18e02d7e25a3bdb9b6d3f1881f8c
Felipe Coury <felipe.coury@gmail.com>
2025-03-24 15:29:06 -0300

    feat(nvim): opens neotree rename in normal mode

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -63,6 +63,15 @@ return {
 						require("neo-tree.command").execute({ action = "close" })
 					end,
 				},
+
+				{
+					event = "neo_tree_popup_input_ready",
+					---@param args { bufnr: integer, winid: integer }
+					handler = function(args)
+						vim.cmd("stopinsert")
+						vim.keymap.set("i", "<esc>", vim.cmd.stopinsert, { noremap = true, buffer = args.bufnr })
+					end,
+				},
 			},
 		})
 	end,
