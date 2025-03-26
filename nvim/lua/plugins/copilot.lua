3dd2a82dfb89a73e3c603bd4a458738d072b324c
Felipe Coury <felipe.coury@gmail.com>
2025-03-26 20:46:19 -0300

    feat: re-enabled avante instead of copilot chat

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -23,24 +23,32 @@ return {
 	-- 		require("copilot_cmp").setup()
 	-- 	end,
 	-- },
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		dependencies = {
-			{ "zbirenbaum/copilot.lua" },
-		},
-		cmd = "CopilotChat",
-		build = "make tiktoken",
-		keys = {
-			{
-				"<leader>ch",
-				"<cmd>CopilotChat<cr>",
-				mode = "n",
-				desc = "Open Copilot Chat",
-				silent = true,
-			},
-		},
-		config = function()
-			require("CopilotChat").setup()
-		end,
-	},
+	-- {
+	-- 	"MeanderingProgrammer/render-markdown.nvim",
+	-- 	optional = true,
+	-- 	opts = {
+	-- 		file_types = { "markdown", "copilot-chat" },
+	-- 	},
+	-- 	ft = { "markdown", "copilot-chat" },
+	-- },
+	-- {
+	-- 	"CopilotC-Nvim/CopilotChat.nvim",
+	-- 	dependencies = {
+	-- 		{ "zbirenbaum/copilot.lua" },
+	-- 	},
+	-- 	cmd = "CopilotChat",
+	-- 	build = "make tiktoken",
+	-- 	keys = {
+	-- 		{
+	-- 			"<leader>ch",
+	-- 			"<cmd>CopilotChat<cr>",
+	-- 			mode = "n",
+	-- 			desc = "Open Copilot Chat",
+	-- 			silent = true,
+	-- 		},
+	-- 	},
+	-- 	config = function()
+	-- 		require("CopilotChat").setup()
+	-- 	end,
+	-- },
 }
