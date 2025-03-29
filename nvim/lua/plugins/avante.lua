4cc80347a19a571799828244af7fba813eb08d54
Felipe Coury <felipe.coury@gmail.com>
2025-03-29 01:25:17 -0300

    feat: uses claude for avante

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -3,17 +3,7 @@ return {
 	event = "VeryLazy",
 	version = false, -- Never set this value to "*"! Never!
 	opts = {
-		-- add any opts here
-		-- for example
-		provider = "openai",
-		openai = {
-			endpoint = "https://api.openai.com/v1",
-			model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-			timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-			temperature = 0,
-			max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-			--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-		},
+		provider = "claude",
 		behaviour = {
 			auto_set_keymaps = false,
 		},
