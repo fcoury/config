ba6f140a2a2043ca654efd961a982aaa834f8950
Felipe Coury <felipe.coury@gmail.com>
2025-09-27 16:59:33 -0300

    feat(nvim): copy works from ssh

---
+++ nvim/lua/plugins/clipboard.lua
@@ -0,0 +1,14 @@
+return {
+	"ojroques/nvim-osc52",
+	opts = { silent = true, trim = true },
+	config = function(_, opts)
+		require("osc52").setup(opts)
+		-- Send normal/visual yanks to local clipboard via OSC52
+		local function copy()
+			if vim.v.event.operator == "y" and vim.v.event.regname == "" then
+				require("osc52").copy_register('"')
+			end
+		end
+		vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
+	end,
+}
