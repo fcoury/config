404eaf52ebed46dfa291ecb2ffb623318a91ab3f
Felipe Coury <felipe.coury@gmail.com>
2025-10-03 19:25:06 -0300

    chore(fish): removed claude alias

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,6 @@ set -gx EDITOR /opt/homebrew/bin/nvim
 set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
 set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
 set -gx TERM "xterm-256color"
-alias claude="/Users/fcoury/.claude/local/claude"
 
 # Added by Windsurf
 set -g PATH $HOME/.codeium/windsurf/bin $PATH
