78c7be962e6a6e7d1c417952160d2c7d4e81cb9e
Felipe Coury <felipe.coury@gmail.com>
2025-12-30 15:03:07 -0300

    feat(scripts): tms to select a remote tmux session to connect to

---
+++ glide/glide.ts
@@ -0,0 +1,19 @@
+// Config docs:
+//
+//   https://glide-browser.app/config
+//
+// API reference:
+//
+//   https://glide-browser.app/api
+//
+// Default config files can be found here:
+//
+//   https://github.com/glide-browser/glide/tree/main/src/glide/browser/base/content/plugins
+//
+// Most default keymappings are defined here:
+//
+//   https://github.com/glide-browser/glide/blob/main/src/glide/browser/base/content/plugins/keymaps.mts
+//
+// Try typing `glide.` and see what you can do!
+glide.keymaps.set(["normal", "visual"], "<C-f>", "scroll_page_down");
+glide.keymaps.set(["normal", "visual"], "<C-b>", "scroll_page_up");
