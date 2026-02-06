fe8b520d4ef66f65f4263498f21311ca32223a99
Felipe Coury <felipe.coury@gmail.com>
2026-02-06 14:40:35 -0300

    feat(nvim): re-enable gitsigns keymaps now that harpoon is gone

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -11,57 +11,58 @@ return {
 				changedelete = { text = "~" },
 			},
 			current_line_blame = false,
-			-- on_attach = function(bufnr)
-			-- local gs = package.loaded.gitsigns
-			--
-			-- local function map(mode, l, r, opts)
-			-- 	opts = opts or {}
-			-- 	opts.buffer = bufnr
-			-- 	vim.keymap.set(mode, l, r, opts)
-			-- end
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
 
-			-- Navigation
-			-- map("n", "]c", function()
-			-- 	if vim.wo.diff then
-			-- 		return "]c"
-			-- 	end
-			-- 	vim.schedule(function()
-			-- 		gs.next_hunk()
-			-- 	end)
-			-- 	return "<Ignore>"
-			-- end, { expr = true })
-			--
-			-- map("n", "[c", function()
-			-- 	if vim.wo.diff then
-			-- 		return "[c"
-			-- 	end
-			-- 	vim.schedule(function()
-			-- 		gs.prev_hunk()
-			-- 	end)
-			-- 	return "<Ignore>"
-			-- end, { expr = true })
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
 
-			-- Actions
-			-- disabled 09-14-2024 due to conflict with Harpoon
-			-- map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
-			-- map({ "n", "v" }, "<leader>hr", ":Gitsigns stage_hunk<CR>")
-			-- map("n", "<leader>hS", gs.stage_buffer)
-			-- map("n", "<leader>ha", gs.stage_hunk)
-			-- map("n", "<leader>hu", gs.undo_stage_hunk)
-			-- map("n", "<leader>hR", gs.reset_buffer)
-			-- map("n", "<leader>hp", gs.preview_hunk)
-			-- map("n", "<leader>hb", function()
-			-- 	gs.blame_line({ full = true })
-			-- end)
-			-- map("n", "<leader>tB", gs.toggle_current_line_blame)
-			-- map("n", "<leader>hd", gs.diffthis)
-			-- map("n", "<leader>hD", function()
-			-- 	gs.diffthis("~")
-			-- end)
+				-- Navigation
+				-- map("n", "]c", function()
+				-- 	if vim.wo.diff then
+				-- 		return "]c"
+				-- 	end
+				-- 	vim.schedule(function()
+				-- 		gs.next_hunk()
+				-- 	end)
+				-- 	return "<Ignore>"
+				-- end, { expr = true })
+				--
+				-- map("n", "[c", function()
+				-- 	if vim.wo.diff then
+				-- 		return "[c"
+				-- 	end
+				-- 	vim.schedule(function()
+				-- 		gs.prev_hunk()
+				-- 	end)
+				-- 	return "<Ignore>"
+				-- end, { expr = true })
 
-			-- Text objects
-			-- map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
-			-- end,
+				-- Actions
+				-- re-enabled 01-31-2026 harpoon is gone
+				-- disabled 09-14-2024 due to conflict with Harpoon
+				map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+				map({ "n", "v" }, "<leader>hr", ":Gitsigns stage_hunk<CR>")
+				map("n", "<leader>hS", gs.stage_buffer)
+				map("n", "<leader>ha", gs.stage_hunk)
+				map("n", "<leader>hu", gs.undo_stage_hunk)
+				map("n", "<leader>hR", gs.reset_buffer)
+				map("n", "<leader>hp", gs.preview_hunk)
+				map("n", "<leader>hb", function()
+					gs.blame_line({ full = true })
+				end)
+				map("n", "<leader>tB", gs.toggle_current_line_blame)
+				map("n", "<leader>hd", gs.diffthis)
+				map("n", "<leader>hD", function()
+					gs.diffthis("~")
+				end)
+
+				-- Text objects
+				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+			end,
 		})
 	end,
 }
