437d69d86be1a33c7bd5f6dc77bb7ec9084a815f
Felipe Coury <felipe.coury@gmail.com>
2026-02-08 02:05:02 -0300

    feat(fish): simplified cdx to not pass in a model

--- fish/conf.d/llm.fish
+++ fish/conf.d/llm.fish
@@ -1,7 +1,8 @@
 # alias cdx='command codex -m gpt-5.1 -c model_reasoning_effort="high" --search --yolo'
 # alias cdx='command codex -m gpt-5.1-codex-max -c model_reasoning_effort="high" --search --yolo'
 # alias codex='command codex -m gpt-5.2 --full-auto -c model_reasoning_effort="medium" -c model_reasoning_summary_format=experimental --search'
-alias cdx='command codex -m gpt-5.2-codex -c model_reasoning_effort="high" --search --yolo'
+#alias cdx='command codex -m gpt-5.3-codex -c model_reasoning_effort="high" --search --yolo'
+alias cdx='command codex --yolo'
 alias cmt="yolo --model sonnet 'Let\'s commit all our pending changes. Check if we need one or more commits and suggest them before committing.'"
 
 function cop
