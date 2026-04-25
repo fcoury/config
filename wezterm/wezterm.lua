e35a06681f3e75a0122a35dab4288de42642b6e1
Felipe Coury <felipe.coury@gmail.com>
2026-04-24 23:10:39 -0300

    chore(terminal): update emulator preferences
    Tune Ghostty, iTerm, WezTerm, and Zellij settings for current
    terminal usage, including font sizing, themes, tabs, and pane layout.
    
    Keep the changes grouped around emulator behavior so editor and shell
    configuration can evolve independently.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -31,6 +31,9 @@ local config = {
 	--
 	-- default_gui_startup_args = { "connect", "unix" },
 
+	-- Enable kitty keyboard mode
+	enable_kitty_keyboard = true,
+
 	-- Set unlimited scrollback
 	scrollback_lines = 35000,
 
@@ -210,9 +213,9 @@ local config = {
 	},
 
 	-- Tabs
-	use_fancy_tab_bar = false,
-	tab_max_width = 24,
-	show_tab_index_in_tab_bar = false,
+	use_fancy_tab_bar = true,
+	tab_max_width = 180,
+	-- show_tab_index_in_tab_bar = true,
 	tab_bar_at_bottom = false,
 	window_frame = {
 		font = wezterm.font({ family = "SF Pro", weight = "Medium" }),
@@ -221,108 +224,110 @@ local config = {
 		inactive_titlebar_bg = "#191919",
 	},
 
+	color_scheme = "One Dark (Gogh)",
+
 	-- Custom theme (lackluster)
-	color_scheme = "Lackluster",
-	colors = {
-		foreground = "#deeeed", -- luster
-		background = "#101010", -- black
-		cursor_bg = "#deeeed", -- luster
-		cursor_border = "#deeeed", -- luster
-		cursor_fg = "#000000", -- black
-		selection_bg = "#444444", -- gray4
-		selection_fg = "#deeeed", -- luster
-
-		ansi = {
-			"#000000", -- black
-			"#D70000", -- red
-			"#789978", -- green
-			"#abab77", -- yellow
-			"#7788AA", -- blue
-			"#ffaa88", -- orange
-			"#708090", -- lack
-			"#aaaaaa", -- gray7
-		},
-		brights = {
-			"#555555", -- gray5
-			"#D70000", -- red
-			"#789978", -- green
-			"#abab77", -- yellow
-			"#7788AA", -- blue
-			"#ffaa88", -- orange
-			"#708090", -- lack
-			"#DDDDDD", -- gray9
-		},
-
-		tab_bar = {
-			background = "#101010",
-
-			active_tab = {
-				bg_color = "#333333",
-				fg_color = "#deeeed",
-				intensity = "Normal",
-				underline = "None",
-				italic = false,
-				strikethrough = false,
-			},
-
-			inactive_tab = {
-				bg_color = "#1a1a1a",
-				fg_color = "#999999",
-				intensity = "Normal",
-				underline = "None",
-				italic = false,
-				strikethrough = false,
-			},
-
-			inactive_tab_hover = {
-				bg_color = "#252525",
-				fg_color = "#cccccc",
-				intensity = "Normal",
-				underline = "None",
-				italic = false,
-				strikethrough = false,
-			},
-
-			new_tab = {
-				bg_color = "#1a1a1a",
-				fg_color = "#999999",
-			},
-
-			new_tab_hover = {
-				bg_color = "#333333",
-				fg_color = "#deeeed",
-				italic = false,
-			},
-		},
-
-		-- tab_bar = {
-		-- 	background = "#191919", -- gray2
-		--
-		-- 	active_tab = {
-		-- 		bg_color = "#444444", -- gray4
-		-- 		fg_color = "#deeeed", -- luster
-		-- 		intensity = "Bold",
-		-- 	},
-		-- 	inactive_tab = {
-		-- 		bg_color = "#191919", -- gray2
-		-- 		fg_color = "#555555", -- gray5
-		-- 	},
-		-- 	inactive_tab_hover = {
-		-- 		bg_color = "#444444", -- gray4
-		-- 		fg_color = "#deeeed", -- luster
-		-- 		italic = true,
-		-- 	},
-		-- 	new_tab = {
-		-- 		bg_color = "#191919", -- gray2
-		-- 		fg_color = "#deeeed", -- luster
-		-- 	},
-		-- 	new_tab_hover = {
-		-- 		bg_color = "#789978", -- green
-		-- 		fg_color = "#191919", -- gray2
-		-- 		italic = true,
-		-- 	},
-		-- },
-	},
+	-- color_scheme = "Lackluster",
+	-- colors = {
+	-- 	foreground = "#deeeed", -- luster
+	-- 	background = "#101010", -- black
+	-- 	cursor_bg = "#deeeed", -- luster
+	-- 	cursor_border = "#deeeed", -- luster
+	-- 	cursor_fg = "#000000", -- black
+	-- 	selection_bg = "#444444", -- gray4
+	-- 	selection_fg = "#deeeed", -- luster
+	--
+	-- 	ansi = {
+	-- 		"#000000", -- black
+	-- 		"#D70000", -- red
+	-- 		"#789978", -- green
+	-- 		"#abab77", -- yellow
+	-- 		"#7788AA", -- blue
+	-- 		"#ffaa88", -- orange
+	-- 		"#708090", -- lack
+	-- 		"#aaaaaa", -- gray7
+	-- 	},
+	-- 	brights = {
+	-- 		"#555555", -- gray5
+	-- 		"#D70000", -- red
+	-- 		"#789978", -- green
+	-- 		"#abab77", -- yellow
+	-- 		"#7788AA", -- blue
+	-- 		"#ffaa88", -- orange
+	-- 		"#708090", -- lack
+	-- 		"#DDDDDD", -- gray9
+	-- 	},
+	--
+	-- 	tab_bar = {
+	-- 		background = "#101010",
+	--
+	-- 		active_tab = {
+	-- 			bg_color = "#333333",
+	-- 			fg_color = "#deeeed",
+	-- 			intensity = "Normal",
+	-- 			underline = "None",
+	-- 			italic = false,
+	-- 			strikethrough = false,
+	-- 		},
+	--
+	-- 		inactive_tab = {
+	-- 			bg_color = "#1a1a1a",
+	-- 			fg_color = "#999999",
+	-- 			intensity = "Normal",
+	-- 			underline = "None",
+	-- 			italic = false,
+	-- 			strikethrough = false,
+	-- 		},
+	--
+	-- 		inactive_tab_hover = {
+	-- 			bg_color = "#252525",
+	-- 			fg_color = "#cccccc",
+	-- 			intensity = "Normal",
+	-- 			underline = "None",
+	-- 			italic = false,
+	-- 			strikethrough = false,
+	-- 		},
+	--
+	-- 		new_tab = {
+	-- 			bg_color = "#1a1a1a",
+	-- 			fg_color = "#999999",
+	-- 		},
+	--
+	-- 		new_tab_hover = {
+	-- 			bg_color = "#333333",
+	-- 			fg_color = "#deeeed",
+	-- 			italic = false,
+	-- 		},
+	-- 	},
+	--
+	-- 	-- tab_bar = {
+	-- 	-- 	background = "#191919", -- gray2
+	-- 	--
+	-- 	-- 	active_tab = {
+	-- 	-- 		bg_color = "#444444", -- gray4
+	-- 	-- 		fg_color = "#deeeed", -- luster
+	-- 	-- 		intensity = "Bold",
+	-- 	-- 	},
+	-- 	-- 	inactive_tab = {
+	-- 	-- 		bg_color = "#191919", -- gray2
+	-- 	-- 		fg_color = "#555555", -- gray5
+	-- 	-- 	},
+	-- 	-- 	inactive_tab_hover = {
+	-- 	-- 		bg_color = "#444444", -- gray4
+	-- 	-- 		fg_color = "#deeeed", -- luster
+	-- 	-- 		italic = true,
+	-- 	-- 	},
+	-- 	-- 	new_tab = {
+	-- 	-- 		bg_color = "#191919", -- gray2
+	-- 	-- 		fg_color = "#deeeed", -- luster
+	-- 	-- 	},
+	-- 	-- 	new_tab_hover = {
+	-- 	-- 		bg_color = "#789978", -- green
+	-- 	-- 		fg_color = "#191919", -- gray2
+	-- 	-- 		italic = true,
+	-- 	-- 	},
+	-- 	-- },
+	-- },
 }
 
 -- smart_splits.apply_to_config(config, {
