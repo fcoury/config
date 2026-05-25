eed01efcc66a3f46caf04fd0924d7db612407433
Felipe Coury <felipe.coury@openai.com>
2026-05-25 15:20:58 -0300

    fix(fish): path pollution on config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -7,18 +7,5 @@ set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
 set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
 set -gx TERM "xterm-256color"
 
-# Added by Windsurf
-set -g PATH $HOME/.codeium/windsurf/bin $PATH
-
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /Users/fcoury/.cache/lm-studio/bin
-# End of LM Studio CLI section
-
 # increase maximum number of open files (for language servers, etc.)
 ulimit -n 65536
-
-# opencode
-fish_add_path /Users/fcoury/.opencode/bin
-
-# fabro
-fish_add_path $HOME/.fabro/bin
