b62c3fb8e3e91829c5db4c76f2e18442534e2f2f
Felipe Coury <felipe.coury@openai.com>
2026-05-26 15:49:32 -0300

    fix(fish): resolve eza after path setup

--- fish/conf.d/exa.fish
+++ fish/conf.d/exa.fish
@@ -1,26 +1,24 @@
-# Remove alias or function named 'ls' if it exists
-functions -e ls
+# Resolve eza when ls is invoked: path.fish is loaded after this fragment.
+function ls
+    if not type -q eza
+        command ls $argv
+        return
+    end
 
-# Check if 'eza' command exists
-if type -q eza || test -e /opt/homebrew/bin/eza
-    # Define the 'ls' function
-    function ls
-        # Check if '-rt' is in the arguments
-        set contains_rt false
-        for arg in $argv
-            if test "$arg" = "-rt"
-                set contains_rt true
-                break
-            end
-        end
-
-        # Replace '-rt' with '-snew' if present
-        if test $contains_rt = true
-            set new_argv (string replace -- '-rt' '-snew' $argv)
-            eza $new_argv
-        else
-            eza $argv
+    # Check if '-rt' is in the arguments
+    set contains_rt false
+    for arg in $argv
+        if test "$arg" = "-rt"
+            set contains_rt true
+            break
         end
     end
-end
 
+    # Replace '-rt' with '-snew' if present
+    if test $contains_rt = true
+        set new_argv (string replace -- '-rt' '-snew' $argv)
+        eza $new_argv
+    else
+        eza $argv
+    end
+end
